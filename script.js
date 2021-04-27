6650fbbfacd2c9e951b21fa069cbb6981246be79
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-04-27 23:37:07 +0200

    function-with-empty-value-and-one-value-fixed

--- script.js
+++ script.js
@@ -2,15 +2,38 @@
 
 A. i'm going to use SPLIT to remove the comma and separate the string-numbers into 3 different strings inside the array
 B. i'm going to reduce combined with parseInt to convert the strings into numbers and to sum them
+
+C. 
  */
 
-function add(string) {
-
-    let splittedArgument = string.split(",",2);
-    let sum = splittedArgument.reduce((total, currentValue) => {
-        return parseInt(total) + parseInt(currentValue);
-    })
+/**
+ * @param {string} stringNumbers
+ */
+function add(stringNumbers) {
+    let sum;
+    // case there is only one number as string inside the string argument like "" or "1" 
+    if(stringNumbers.length <= 1) {
+       sum = oneArgument(stringNumbers);
+    } else {
+    //case there are more than one number as string inside the string argument like "1,2"
+        let splittedArgument = string.split(",",2);
+        sum = splittedArgument.reduce((total, currentValue) => {
+            return parseInt(total) + parseInt(currentValue);
+        })
+    }
     return sum;
 }
 
-console.log(add(""));
+function oneArgument(stringNumbers) {
+    // if the string value is equal to "" the stringNumbers is equal to 0
+    if(stringNumbers === "") {
+        stringNumbers = 0;
+        sum = stringNumbers;
+    } else {
+    // if there is only one number passed as argument than stringNumbers is equal converted into a number
+        sum = parseInt(stringNumbers);
+    }
+    return sum;
+}
+
+console.log(add("1,2"));
