ef2fa22fa07de6c13a1ddf13bc89fa6cb29e3206
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-04-29 22:02:02 +0200

    second-test-one-argument-added

--- test/test.js
+++ test/test.js
@@ -6,4 +6,7 @@ describe('step1', function() {
     it('should return 0 if empty argument', function() {
       assert.equal(stringCalculator.add(""),0);
     })
+    it('should return the number passed as argument, here 1', function() {
+        assert.equal(stringCalculator.add("1"),1);
+      })
 })
